e2927dc6ad258459d2865db4f2c6e61d1f2b8355
MaximZemskov <m.zemskov1@gmail.com>
2016-04-07 20:45:11 +0300

    add file respond

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from time import strftime, gmtime
 
 from eventlet.green import socket as green_socket
 from eventlet.green import os as green_os
-from server_utils import get_date
+from server_utils import *
 
 import os
 import socket
@@ -11,33 +11,62 @@ import urllib
 SERVER = "crzINX. Zemskov Maxim"
 CONNECTION = "keep-alive"
 
+
 def parse_request(c):
     headers = c.split('\r\n')
     first_line = headers[0].split(' ')
     method = first_line[0]
     url = first_line[1]
-    print method
-    print url
     url = urllib.unquote(url)
     url = os.path.normpath(url)
-    return url, method
+    split_url = url.split(".")
+    if len(split_url) == 2:
+        content_type = split_url[-1]
+        return url, method, content_type
+    elif len(split_url) == 1:
+        content_type = "html"
+        return url, method, content_type
+    content_type = "bad"
+    return url, method, content_type
 
 
-def index_respond(url):
+def respond(http_version, status, content_length, content_type):
+    http_response = "{} {}".format(http_version, status) + '\r\n'
+    http_response += "Date: " + get_date() + '\r\n'
+    http_response += "Sever: {}".format(SERVER) + '\r\n'
+    http_response += "Content-Length: {}".format(content_length) + '\r\n'
+    http_response += "Connection: {}".format(CONNECTION) + '\r\n'
+    http_response += "Content-Type: {}".format(content_type) + '\r\n'
+    http_response += '\r\n'
+    return http_response
+
+
+def index_respond(url, content_type):
     uri = url + "/index.html"
     if os.path.isfile(uri):
         f = open(uri, "rb")
-        http_response = "Date: " + get_date() + '\r\n'
-        http_response += "Sever: {}".format(SERVER) + '\r\n'
-        http_response += "Content-Length: {}".format(os.path.getsize(uri)) + '\r\n'
-        # http_response += "Content-Type: {}".format() + '\r\n'
-        http_response += "Connection: {}".format(CONNECTION) + '\r\n'
+        http_version = "HTTP/1.1"
+        status = "200 OK"
+        content_length = os.path.getsize(uri)
+        http_response = respond(http_version, status, content_length, content_type)
         return f, http_response
     else:
-        respond_404(uri)
+        respond_403(uri)
 
 
-def file_respond(url):
+def file_respond(url, content_type):
+    if os.path.isfile(url):
+        f = open(url, "rb")
+        http_version = "HTTP/1.1"
+        status = "200 OK"
+        content_length = os.path.getsize(url)
+        http_response = respond(http_version, status, content_length, content_type)
+        return f, http_response
+    else:
+        respond_404()
+
+
+def respond_403(url):
     pass
 
 
@@ -51,19 +80,22 @@ def handle(client):
     c = client.recv(1024)
     print c
     try:
-        url, method = parse_request(c)
+        url, method, content_type = parse_request(c)
+        content_type = check_content_type(content_type)
         if os.path.isdir(root_dir + url):
-            body, headers = index_respond(root_dir + url)
+            body, headers = index_respond((root_dir + url), content_type)
             client.sendall(headers)
             if body:
-                l = body.read(4096)
-                while l:
-                    client.send(l)
-                    l = body.read(4096)
+                data_send(client, body)
                 body.close()
             print "Directory exist"
         elif os.path.isfile(root_dir + url):
-            # file_respond
+            body, headers = file_respond((root_dir + url), content_type)
+            client.sendall(headers)
+            if body:
+                data_send(client, body)
+                body.close()
+                # TODO: DRY
             print "File exist"
         else:
             # respond_404
--- server/server_utils.py
+++ server/server_utils.py
@@ -1,5 +1,30 @@
 import time
 
+CONTENT_TYPE = {
+    'txt': 'text/html',
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'application/javascript',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif',
+    'swf': 'application/x-shockwave-flash',
+    'other': 'application/octet- stream',
+}
+
 
 def get_date():
     return time.strftime("%a, %d %b %Y %X %Z")
+
+
+def check_content_type(type):
+    if type in CONTENT_TYPE:
+        return CONTENT_TYPE[type]
+
+
+def data_send(client, body):
+    l = body.read(4096)
+    while l:
+        client.send(l)
+        l = body.read(4096)
